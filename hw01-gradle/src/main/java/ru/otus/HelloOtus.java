6de9ca2040a7951cdeaebceb0e3925c9c13c7cde
vetalspb <vetalspb@inbox.ru>
2023-11-28 13:40:18 +0300

    spotless

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -5,11 +5,8 @@ import com.google.common.base.Splitter;
 @SuppressWarnings("java:S106")
 public class HelloOtus {
     public static void main(String... args) {
-        Iterable<String> strings = Splitter.on(',')
-                                           .trimResults()
-                                           .omitEmptyStrings()
-                                           .split("abc, def,, ghi   ,jkl");
+        Iterable<String> strings =
+                Splitter.on(',').trimResults().omitEmptyStrings().split("abc, def,, ghi   ,jkl");
         System.out.println(strings);
     }
-
 }
